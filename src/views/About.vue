4dd75efc9f7996c26f860869bea3e5139acdacfb
Aeorae <services.fedorae@gmail.com>
2021-08-15 23:40:34 +0100

    Vue3 Router

---
+++ src/views/About.vue
@@ -0,0 +1,5 @@
+<template id="">
+  <div class="">
+    About
+  </div>
+</template>
---
+++ src/views/Home.vue
@@ -0,0 +1,8 @@
+<template id="">
+  <div class="">
+    Root 1
+  </div>
+  <div class="">
+    Root 2
+  </div>
+</template>
